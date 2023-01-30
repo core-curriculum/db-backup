0ff9e45f1e31c04bd4028ba9a92a8033219b987d
Takeshi Kondo <ncukondo@gmail.com>
2023-01-30 08:51:16 +0000

    fix dependency of supabase and add Deno.exit()

--- main.ts
+++ main.ts
@@ -1,21 +1,30 @@
-import { createClient } from 'supabase'
+import { createClient } from "supabase";
 import { config } from "./lib/config.ts";
 
 const { SUPABASE_URL, SUPABASE_KEY } = config;
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
-const accumulateCsv = async (step: number, getFn: (start: number, end: number) => Promise<string[]>): Promise<string[]> => {
+const accumulateCsv = async (
+  step: number,
+  getFn: (start: number, end: number) => Promise<string[]>,
+): Promise<string[]> => {
   const result: string[] = [];
   let header = "";
   let pos = 0;
   while (true) {
-    const [_header, ...body] = await getFn(pos, pos + step);
+    try {
+      const [_header, ...body] = await getFn(pos, pos + step);
+      if (!header) header = _header;
+      if (!body || body.length === 0 || body.join("") === "") {
+        return [header, ...result];
+      }
+      result.push(...body);
+    } catch (e) {
+      console.error(e);
+    }
     pos = pos + step + 1;
-    if (!header) header = _header;
-    if (!body || body.length === 0 || body.join("") === "") return [header, ...result];
-    result.push(...body);
   }
-}
+};
 
 const getDataCsv = async (table: string, step = 500) => {
   const getFunc = async (start: number, end: number) => {
@@ -23,20 +32,22 @@ const getDataCsv = async (table: string, step = 500) => {
       .from(table)
       .select()
       .range(start, end)
-      .csv()
+      .csv();
 
-    if (error || !data) throw error ?? new Error(`Error getting ex_data ${error}`);
+    if (error || !data) {
+      throw error ?? new Error(`Error getting ex_data ${error}`);
+    }
     return data.split("\n");
-  }
-  return await accumulateCsv(step, getFunc)
-}
+  };
+  return await accumulateCsv(step, getFunc);
+};
 
 const dbToCsv = async (name: string) => {
   const data = await getDataCsv(name);
   const csv = data.join("\n");
-  await Deno.writeTextFile(`./output/${name}.csv`, csv)
-}
+  await Deno.writeTextFile(`./output/${name}.csv`, csv);
+};
 
-await dbToCsv("ex_data")
-await dbToCsv("item_list")
-
+await dbToCsv("ex_data");
+await dbToCsv("item_list");
+Deno.exit();
